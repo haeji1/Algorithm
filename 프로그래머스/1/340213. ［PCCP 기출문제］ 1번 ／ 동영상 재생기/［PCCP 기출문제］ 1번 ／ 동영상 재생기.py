fd87eec77652d6b350dc79e4bf72c77898db8f02
🎀혜찌🎀 <73926423+haeji1@users.noreply.github.com>
2025-03-17 19:56:47 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.08 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -5,36 +5,47 @@ def solution(video_len, pos, op_start, op_end, commands):
     video_mm, video_ss = map(int,video_len.split(":"))
     
      # 시작할 때 오프닝 구간인 경우 고려
-    if (op_start_mm < pos_mm == op_end_mm and pos_ss < op_end_ss) or \
-        (op_start_mm < pos_mm < op_end_mm) or \
-        (op_start_mm == pos_mm == op_end_mm and op_start_ss <= pos_ss <= op_end_ss) or \
-        (op_start_mm == pos_mm < op_end_mm and op_start_ss <= pos_ss):
-            pos_mm, pos_ss = op_end_mm, op_end_ss
+    if (op_start_mm < pos_mm < op_end_mm or
+        op_start_mm != op_end_mm and op_start_mm == pos_mm and pos_ss >= op_start_ss or
+        op_start_mm == pos_mm == op_end_mm and op_start_ss <= pos_ss < op_end_ss or
+        op_start_mm != op_end_mm and pos_mm == op_end_mm and pos_ss < op_end_ss):
+            pos_mm = op_end_mm
+            pos_ss = op_end_ss
     
-    
-    for c in commands:     
+    print(pos_mm, pos_ss)
+    for c in commands:    
         # 1. prev
         if c == 'prev':
             if pos_ss - 10 < 0 and pos_mm > 0:
                 pos_mm -= 1
-                pos_ss = 60 + (pos_ss - 10)
+                pos_ss = (pos_ss + 60) - 10
             else:
                 pos_ss = max(0, pos_ss - 10)
                 
         # 2. next
         elif c == 'next':
-            if pos_ss + 10 >= 60 and pos_mm < video_mm:
+            pos_ss += 10
+            if pos_ss >= 60:
+                pos_ss -= 60
                 pos_mm += 1
-            if pos_mm == video_mm:
-                pos_ss = min((pos_ss + 10) % 60, video_ss)
-            else:
-                pos_ss = (pos_ss + 10) % 60
- 
-        # 연산을 마친 후 오프닝 구간인 경우 고려
-        if (op_start_mm < pos_mm == op_end_mm and pos_ss < op_end_ss) or \
-        (op_start_mm < pos_mm < op_end_mm) or \
-        (op_start_mm == pos_mm == op_end_mm and op_start_ss <= pos_ss <= op_end_ss) or \
-        (op_start_mm == pos_mm < op_end_mm and op_start_ss <= pos_ss):
-            pos_mm, pos_ss = op_end_mm, op_end_ss
-
-    return str(pos_mm).zfill(2) + ":"+str(pos_ss).zfill(2)
+                
+            if pos_mm > video_mm:
+                pos_mm = video_mm
+                pos_ss = video_ss
+            
+            if pos_mm == video_mm and pos_ss > video_ss:
+                pos_ss = video_ss
+                
+        if (op_start_mm < pos_mm < op_end_mm or
+        op_start_mm != op_end_mm and op_start_mm == pos_mm and pos_ss >= op_start_ss or
+        op_start_mm == pos_mm == op_end_mm and op_start_ss <= pos_ss < op_end_ss or
+        op_start_mm != op_end_mm and pos_mm == op_end_mm and pos_ss < op_end_ss):
+            pos_mm = op_end_mm
+            pos_ss = op_end_ss
+            
+    pos_mm = str(pos_mm)
+    pos_ss = str(pos_ss)
+    pos_mm = pos_mm.zfill(2)
+    pos_ss = pos_ss.zfill(2)
+    answer = pos_mm +":"+ pos_ss
+    return answer
